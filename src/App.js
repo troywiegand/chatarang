d7431f7258f68024b6dd22c99d202a9ebae4158e
Troy <troy@troywiegand.com>
2018-07-03 09:39:22 -0400

    Introduce `Main` component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+
 import './App.css'
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>Chatarang</h1>
+       <Main />
       </div>
     )
   }
