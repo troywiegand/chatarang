b5322f617b778f03c23ebcae40009229ac3206e1
Troy <troy@troywiegand.com>
2018-07-03 10:10:08 -0400

    Introduce ChatHeader

---
+++ src/ChatHeader.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class ChatHeader extends Component{
+render(){
+    return(
+
+    <header class="ChatHeader">
+        <div class="roomInfo">
+          <h2>#general</h2>
+          <p>Announcements and general chat</p>
+        </div>
+      </header>
+    )
+}
+
+}
+
+export default ChatHeader
