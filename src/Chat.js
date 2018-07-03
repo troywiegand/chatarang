b7aabb6973afb50822ac887528f449dc9aa92eb1
Troy <troy@troywiegand.com>
2018-07-03 09:42:18 -0400

    Introduce Sidebar and Chat components

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Chat extends Component{
+render(){
+    return(
+        <div className="Chat">
+            Chat
+        </div>
+    )
+}
+
+}
+
+export default Chat
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+
+
 class Main extends Component{
 render(){
     return(
         <div className="main">
             Main
+            <Sidebar />
+            <Chat />
         </div>
     )
 }
