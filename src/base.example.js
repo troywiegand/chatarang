e88db39f0a2bcec2d356032d6f02bc2ff0822287
Troy <troy@troywiegand.com>
2018-07-09 09:39:06 -0400

    Update base.js to enable auth

--- src/base.example.js
+++ src/base.example.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import Rebase from 're-base'
+import database from 'firebase/database'
 
 // Initialize Firebase
 const config = {
@@ -11,3 +13,10 @@ const config = {
 }
 
 const app = firebase.initializeApp(config)
+const db = firebase.database(app)
+
+export const auth = firebase.auth()
+export const googleProvider= new firebase.auth.GoogleAuthProvider()
+
+
+export default Rebase.createClass(db)
